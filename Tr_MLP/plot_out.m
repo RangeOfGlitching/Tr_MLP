e7fa781443e22afb43701726d078b6dac7a8c2d5
RangeOfGlitching <58881058+RangeOfGlitching@users.noreply.github.com>
2022-03-23 21:16:59 +0800

    Update plot_out.m

--- Tr_MLP/plot_out.m
+++ Tr_MLP/plot_out.m
@@ -1,6 +1,7 @@
 clear;
 load out.dat
 load act_y.dat
+load RMSE.dat
 plot(out, 'o');
 hold;
 plot(act_y,'x');
@@ -9,3 +10,5 @@ dim= size(out);
 (sum(err.^2)/dim(1,1))^0.5
 
 
+figure(2)
+plot(RMSE);
